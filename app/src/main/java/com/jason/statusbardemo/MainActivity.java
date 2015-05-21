c3add932528004f757ae9a99d111ea63bab0bdf0
吴征同学 <wuzheng0420@126.com>
2015-05-21 17:44:03 +0800

    提交

---
+++ app/src/main/java/com/jason/statusbardemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jason.statusbardemo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //设定状态栏的颜色，当版本大于4.4时起作用
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintResource(R.color.primary_dark);
+        }
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("主标题");// 标题的文字需在setSupportActionBar之前，不然会无效
+        setSupportActionBar(mToolbar);
+
+    }
+}
